e2b842458659f2970e9ba636dec791e0a6dca8e9
Andres <andresgallegosluque@gmail.com>
2022-09-25 22:17:29 -0500

    Servicio de registro de autor completado en la parte Back

--- src/main/java/com/cibertec/controller/AutorController.java
+++ src/main/java/com/cibertec/controller/AutorController.java
@@ -8,15 +8,67 @@ package com.cibertec.controller;
 
  */
 
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import com.cibertec.entity.Autor;
+import com.cibertec.service.AutorService;
 import com.cibertec.util.AppSettings;
 
 @RestController
 @RequestMapping("/url/autor")
 @CrossOrigin(origins = AppSettings.URL_CROSS_ORIGIN)
 public class AutorController {
+	
+	@Autowired
+	private AutorService autorService;
+	
+	@PostMapping
+	@ResponseBody
+	public ResponseEntity<?> inserta(@Valid @RequestBody Autor obj, Errors errors){
+		
+		HashMap<String, Object> salida = new HashMap<>();
+	
+	
+			List<String> lstMensajes = new ArrayList<String>();
+			salida.put("errores", lstMensajes);
+			
+			List<ObjectError> lstErrors =  errors.getAllErrors();
+			for (ObjectError objectError : lstErrors) {
+				objectError.getDefaultMessage();
+				lstMensajes.add(objectError.getDefaultMessage());
+			}
+
+			if (!CollectionUtils.isEmpty(lstMensajes)) {
+				return ResponseEntity.ok(salida);
+			}
+			
+			obj.setFechaRegistro(new Date());
+			obj.setEstado(1);
+			Autor objSalida = autorService.insertaAutor(obj);
+			if (objSalida == null) {
+				lstMensajes.add("Error en el registro");
+			}else {
+				lstMensajes.add("Se registró el autor con el ID ==> " + objSalida.getIdAutor());
+			}
+		
+		return ResponseEntity.ok(salida);
+	}
 
 }
---
+++ src/main/java/com/cibertec/service/AutorService.java
@@ -0,0 +1,9 @@
+package com.cibertec.service;
+
+import com.cibertec.entity.Autor;
+
+public interface AutorService {
+
+	public abstract Autor insertaAutor(Autor obj);
+	
+}
