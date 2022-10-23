355d0f110f55f674cff3ae98f84348de0065aae9
Miguel <minekout80@gmail.com>
2022-10-23 13:08:45 -0500

    Servicio para la consulta de Sala por 5 campos

--- src/main/java/com/cibertec/service/SalaService.java
+++ src/main/java/com/cibertec/service/SalaService.java
@@ -9,6 +9,7 @@ public interface SalaService {
 
 	public abstract List<Sala> listaTodos();
 	public Sala insertaSala(Sala obj);
+	public abstract List<Sala>listaSalaPorCampos(String numero,String recursos, String fecInicio, String fecFin,int estado,int idSede);
 	
 
 }
